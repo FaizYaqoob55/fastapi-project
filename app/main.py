88aa7c8c5046377bdeceeb65f0ebcf49d0c61952
FaizYaqoob55 <faizyaqoob55@gmail.com>
2026-02-10 16:23:22 +0500

    Update main.py
    l

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="My FastAPI Application")
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app',host="0.0.0.0",port=8000, reload=True)
