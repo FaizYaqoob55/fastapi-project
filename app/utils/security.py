7a3a802b70c44e40f0f854578a035986712a0ebf
FaizYaqoob55 <faizyaqoob55@gmail.com>
2026-02-16 12:25:53 +0500

    full jwt setup

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,4 +1,15 @@
+import os
 import bcrypt
+from datetime import datetime, timedelta
+import jwt
+# from .env import SECRET_KEY as SeCRET_KEY
+from dotenv import load_dotenv
+
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
 
 def hash_password(password:str):
     pwd_bytes=password.encode('utf-8')
@@ -11,3 +22,24 @@ def verify_password(plain_password:str , hashed_password:str):
         plain_password.encode('utf_8'),
         hashed_password.encode('utf_8')
     )
+
+
+def create_access_token(data:dict,expires_delta:timedelta |None=None):
+    to_encode=data.copy()
+    if expires_delta:
+        expire=datetime.utcnow()+expires_delta
+    else:
+        expire=datetime.utcnow()+timedelta(minutes=15)
+    to_encode.update({'exp':expire})
+    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
+
+def refresh_access_token(data:dict,expires_delta:timedelta |None=None):
+    to_encode=data.copy()
+    if expires_delta:
+        expire=datetime.utcnow()+expires_delta
+    else:
+        expire=datetime.utcnow()+timedelta(days=7)
+    to_encode.update({'exp':expire})
+    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
